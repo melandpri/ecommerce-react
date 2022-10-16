a3d178ed9e44871b5099b7353c6ba044da9afe24
Fogue <foguelilia97@gmail.com>
2022-10-16 19:35:44 +0100

    déclaration des usestates

--- src/App.js
+++ src/App.js
@@ -9,19 +9,48 @@ import { useState } from 'react';
 
 function App() {
 
-  const  [cartCount ,setCartCount ] = useState(0);
+  const [cartCount, setCartCount] = useState(0);
 
-  const  [cartItem ,setCartItem ] = useState([]);
+  let  [cartItem, setCartItem] = useState([]);//contient tous les artiches qui sont ajoutés dans notre panier 
+
+  const addProduct = (clickedProduct) => {
+
+    const updatedList = cartItem.map((product) => {
+
+      if (product.id === clickedProduct.id) {
+        return {
+          ...product, quantity: product.quantity += 1
+        }
+
+      }
+
+    })
+    setCartItem(updatedList)
+  }
+  const decreaseProduct = (clickedProduct) => {
+
+    const updatedList = cartItem.map((product) => {
+
+      if (product.id === clickedProduct.id) {
+        return {
+          ...product, quantity: product.quantity -= 1
+        }
+
+      }
+
+    })
+    setCartItem(updatedList)
+  }
 
 
   return (
     <div className="App">
-      
-       <Header count={cartCount}/>
-       <Heros/>
-       <Shop count={cartCount} setCount={setCartCount} item={cartItem} setItem={setCartItem}/>
-       <Featured/>
-       <Footer/>
+
+      <Header count={cartItem} incre={cartItem}  setincre={setCartItem} incrementProduct={addProduct}  deProduct={decreaseProduct}/>
+      <Heros />
+      <Shop count={cartCount} setCount={setCartCount} item={cartItem} setItem={setCartItem} />
+      <Featured />
+      <Footer />
 
     </div>
   );
